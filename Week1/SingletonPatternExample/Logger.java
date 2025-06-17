40ede27b3338bcc338f4ae2d64e0e4299f26dc29
Rohith Enduluri <e.reddyrohith2004@gmail.com>
2025-06-17 16:50:22 +0530

    Enhance Logger class with additional logging statements for instance creation and message recording

--- Week1/SingletonPatternExample/Logger.java
+++ Week1/SingletonPatternExample/Logger.java
@@ -5,16 +5,19 @@ public class Logger {
 
     private Logger() {
         // Private constructor to prevent instantiation
+        System.out.println("Logger instance created.");
     }
 
     public static Logger getInstance() {
         if (instance == null) {
             instance = new Logger();
+            System.out.println("Logger instance initialized.");
         }
         return instance;
     }
 
     public void log(String message) {
         System.out.println(message);
+        System.out.println("Log message recorded.");
     }
 }
