553a41a2e7f70b00412bbb0286ea41c967bc79b6
Rohith Enduluri <e.reddyrohith2004@gmail.com>
2025-07-21 15:12:01 +0530

    Spring Web Project

---
+++ 4_SpringRESTusingSpringBoot/1_SpringWebProject/Code/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting Spring Learn Application...");
+		SpringApplication.run(SpringLearnApplication.class, args);
+		System.out.println("Spring Learn Application started successfully!");
+	}
+
+}
