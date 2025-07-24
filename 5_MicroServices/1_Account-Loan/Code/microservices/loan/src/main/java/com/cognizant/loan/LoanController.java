461580c57df7ea0e3189b189f94b800b5dfd4d2c
Rohith Enduluri <e.reddyrohith2004@gmail.com>
2025-07-24 10:41:13 +0530

    Microservices

---
+++ 5_MicroServices/1_Account-Loan/Code/microservices/loan/src/main/java/com/cognizant/loan/LoanController.java
@@ -0,0 +1,15 @@
+package com.cognizant.loan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Loan getLoan(@PathVariable String number) {
+        // Return dummy loan data without backend connectivity
+        return new Loan(number, "car", 400000.0, 3258.0, 18);
+    }
+}
